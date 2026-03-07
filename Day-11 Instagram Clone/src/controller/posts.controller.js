6950d32fb89bc9bbed62d0f39f0aafbf028a105b
AVISHEK DUTTA <109311355+logicbyavishek@users.noreply.github.com>
2026-03-07 19:31:56 +0530

    save post detail in database

--- Day-11 Instagram Clone/src/controller/posts.controller.js
+++ Day-11 Instagram Clone/src/controller/posts.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/posts.model")
 const ImageKit = require("@imagekit/nodejs")
 const { toFile } = require("@imagekit/nodejs")
+const jwt = require("jsonwebtoken") // require token because identify which user create the post
 
 const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
@@ -9,14 +10,46 @@ const imagekit = new ImageKit({
 async function createPostController(req,res) {
     console.log(req.body, req.file)
 
+    const token = req.cookies.token // fetch the token to findout the user
+    if(!token){
+        return res.status(401).json({
+            Message:"Token not provided , Unauthorized access"
+        })
+    }
+    let decoded = null
+    try {
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+    } catch (error) {
+        return res.status(401).json({
+            Message:"user not authorized"
+        })
+    }
+    console.log(decoded)
+
+
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
-        fileName: "Test"
+        fileName: "Test",
+        folder:"Instagram-Clone"
     })
 
-    res.send(file)
+    const post = await postModel.create({
+        caption:req.body.caption,
+        imgUrl:file.url,
+        user:decoded.id
+    })
+
+    res.status(201).json({
+        Message:"Post created succesfully",
+        post
+    })
+}
+
+async function getPostController(req,res){
+    
 }
 
 module.exports={
     createPostController
 }
+
--- Day-11 Instagram Clone/src/routes/posts.routes.js
+++ Day-11 Instagram Clone/src/routes/posts.routes.js
@@ -11,7 +11,15 @@ const upload = multer({storage:multer.memoryStorage()})
  * req.body{caption , image-file}
  **/
 
+// Multer use for a middleware and it's used to accept the file from uder side express can't read file so that'as why we use middleware
+
 postRouter.post("/",upload.single("Image"),postController.createPostController)
+
+/**
+ * GET /api/posts/ [protected]
+ */
+
+postRouter.get("/",postController)
 
 
 module.exports=postRouter
